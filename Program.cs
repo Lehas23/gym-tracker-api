b4e6e8a26676ac6c8b0266cd7ac9a4591394afdd
Leart <le.hasanaj@gmail.com>
2026-04-29 11:51:30 +0200

    enable swagger in production

--- Program.cs
+++ Program.cs
@@ -55,11 +55,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("AllowFrontend");
 
